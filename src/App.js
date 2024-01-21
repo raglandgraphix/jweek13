dd2e93fb8db9aaccc87e4b18188a62fc4d707d33
John M. Ragland <john@raglandgraphix.com>
2024-01-20 21:37:54 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,23 +6,18 @@ import Nav from './Nav';
 function App() {
   return (
     <div className="App">
+      {/* adds the Nav */}
      <Nav></Nav>
+     {/* adds the h3 */}
      <div id='login'>
-      <h3>Log In</h3>
-      
-      
+      <h3>Log In</h3>     
      </div>
+     {/* adds the log in form */}
      <div id='log'>
       <label>Username:</label><input type='text' ></input>
-       <label>Password</label><input type='text'></input>
+      <label>Password</label><input type='text'></input>
      </div>
-       
-            
-          
-       
-           
-          
-      </div>
+    </div>
    
   );
 }
